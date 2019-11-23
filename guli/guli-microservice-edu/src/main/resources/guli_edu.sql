fbc88f80008552d904a625cb05d9e4f81bf6a3f3
wangchao <wangchao>
2019-11-23 11:40:28 +0800

    数据库脚本

---
+++ guli/guli-microservice-edu/src/main/resources/guli_edu.sql
@@ -0,0 +1,133 @@
+/*
+SQLyog Enterprise v12.09 (64 bit)
+MySQL - 5.7.17-log : Database - guli_edu
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`guli_edu` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `guli_edu`;
+
+/*Table structure for table `edu_chapter` */
+
+DROP TABLE IF EXISTS `edu_chapter`;
+
+CREATE TABLE `edu_chapter` (
+  `id` char(19) NOT NULL COMMENT '章节ID',
+  `course_id` char(19) NOT NULL COMMENT '课程ID',
+  `title` varchar(50) NOT NULL COMMENT '章节名称',
+  `sort` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '显示排序',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_course_id` (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='课程';
+
+/*Table structure for table `edu_course` */
+
+DROP TABLE IF EXISTS `edu_course`;
+
+CREATE TABLE `edu_course` (
+  `id` char(19) NOT NULL COMMENT '课程ID',
+  `teacher_id` char(19) NOT NULL COMMENT '课程讲师ID',
+  `subject_id` char(19) NOT NULL COMMENT '课程专业ID',
+  `subject_parent_id` char(19) NOT NULL COMMENT '课程专业父级ID',
+  `title` varchar(50) NOT NULL COMMENT '课程标题',
+  `price` decimal(10,4) unsigned NOT NULL DEFAULT '0.0000' COMMENT '课程销售价格，设置为0则可免费观看',
+  `lesson_num` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '总课时',
+  `cover` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '课程封面图片路径',
+  `buy_count` bigint(10) unsigned NOT NULL DEFAULT '0' COMMENT '销售数量',
+  `view_count` bigint(10) unsigned NOT NULL DEFAULT '0' COMMENT '浏览数量',
+  `version` bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+  `status` varchar(10) NOT NULL DEFAULT 'Draft' COMMENT '课程状态 Draft未发布  Normal已发布',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_title` (`title`),
+  KEY `idx_subject_id` (`subject_id`),
+  KEY `idx_teacher_id` (`teacher_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='课程';
+
+/*Table structure for table `edu_course_description` */
+
+DROP TABLE IF EXISTS `edu_course_description`;
+
+CREATE TABLE `edu_course_description` (
+  `id` char(19) NOT NULL COMMENT '课程ID',
+  `description` text COMMENT '课程简介',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='课程简介';
+
+/*Table structure for table `edu_subject` */
+
+DROP TABLE IF EXISTS `edu_subject`;
+
+CREATE TABLE `edu_subject` (
+  `id` char(19) NOT NULL COMMENT '课程类别ID',
+  `title` varchar(10) NOT NULL COMMENT '类别名称',
+  `parent_id` char(19) NOT NULL DEFAULT '0' COMMENT '父ID',
+  `sort` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '排序字段',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_parent_id` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='课程科目';
+
+/*Table structure for table `edu_teacher` */
+
+DROP TABLE IF EXISTS `edu_teacher`;
+
+CREATE TABLE `edu_teacher` (
+  `id` char(19) NOT NULL COMMENT '讲师ID',
+  `name` varchar(20) NOT NULL COMMENT '讲师姓名',
+  `intro` varchar(255) NOT NULL COMMENT '讲师资历,一句话说明讲师',
+  `career` text COMMENT '讲师简介',
+  `level` int(10) unsigned NOT NULL COMMENT '头衔 1高级讲师 2首席讲师',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '讲师头像',
+  `sort` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '逻辑删除 1（true）已删除， 0（false）未删除',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='讲师';
+
+/*Table structure for table `edu_video` */
+
+DROP TABLE IF EXISTS `edu_video`;
+
+CREATE TABLE `edu_video` (
+  `id` char(19) NOT NULL COMMENT '视频ID',
+  `course_id` char(19) NOT NULL COMMENT '课程ID',
+  `chapter_id` char(19) NOT NULL COMMENT '章节ID',
+  `title` varchar(50) NOT NULL COMMENT '节点名称',
+  `video_source_id` varchar(100) DEFAULT NULL COMMENT '云端视频资源',
+  `video_original_name` varchar(100) DEFAULT NULL COMMENT '原始文件名称',
+  `sort` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '排序字段',
+  `play_count` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '播放次数',
+  `is_free` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否可以试听：0收费 1免费',
+  `duration` float NOT NULL DEFAULT '0' COMMENT '视频时长（秒）',
+  `status` varchar(20) NOT NULL DEFAULT '' COMMENT '视频状态',
+  `size` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '视频源文件大小（字节）',
+  `version` bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_course_id` (`course_id`),
+  KEY `idx_chapter_id` (`chapter_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='课程视频';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
